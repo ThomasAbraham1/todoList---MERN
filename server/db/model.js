d8facde8ddbcb7e815be7d3bba769a4c8b66d294
ThomasAbraham1 <74496765+ThomasAbraham1@users.noreply.github.com>
2024-04-12 18:23:13 +0530

    Update model.js
    Added cloud connection to Mongo DB

--- server/db/model.js
+++ server/db/model.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://127.0.0.1:27017/necromancers');
+// mongoose.connect('mongodb://127.0.0.1:27017/necromancers');
+mongoose.connect("mongodb+srv://Thomas:0ktPEdzdt6HdDqiH@cluster0.3q3bb.mongodb.net/todoList-MERN");
 const usersSchema = new mongoose.Schema({
     item: String,
 })
