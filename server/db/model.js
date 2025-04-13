5eccce62ac288992877d3e663eb8b26ba68c1283
ThomasAbraham1 <74496765+ThomasAbraham1@users.noreply.github.com>
2025-04-13 17:23:41 +0530

    commit

--- server/db/model.js
+++ server/db/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/necromancers');
-mongoose.connect("mongodb+srv://Thomas:0ktPEdzdt6HdDqiH@cluster0.3q3bb.mongodb.net/todoList-MERN");
+mongoose.connect("mongodb+srv://Thomas:0ktPEdzdt6HdDqiH@cluster0.qesx1ag.mongodb.net/todoList-MERN");
 const usersSchema = new mongoose.Schema({
     item: String,
 })
