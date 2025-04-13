7c9f03d9721c7cf60e0fcb6b7ac6d52d0646a013
ThomasAbraham1 <74496765+ThomasAbraham1@users.noreply.github.com>
2025-04-13 17:28:16 +0530

    commit 5

--- server/db/model.js
+++ server/db/model.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 
 // mongoose.connect('mongodb://127.0.0.1:27017/necromancers');
-mongoose.connect("mongodb+srv://Thomas:Titusbonkey1@cluster0.qesx1ag.mongodb.net/todoList-MERN");
+mongoose.connect("mongodb+srv://cta102938:Titusbonkey1@cluster0.qesx1ag.mongodb.net/todoList-MERN");
 const usersSchema = new mongoose.Schema({
     item: String,
 })
